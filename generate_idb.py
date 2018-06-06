e0a771c3d4ab6d69041b90c0ae4d4d13d2b27db6
mayuan <mayuanucas@qq.com>
2018-06-06 11:24:49 +0800

    支持指定启动ida脚本位置

--- generate_idb.py
+++ generate_idb.py
@@ -21,18 +21,21 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-def main(input_file, output_file):
+def main(input_file, output_file, ida_start=IDA_START, ida_engine_path=IDA_ENGINE_PATH):
     if os.path.isfile(input_file):
         try:
-            generate_i64(input_file, output_file)
+            generate_i64(input_file, output_file, ida_start, ida_engine_path)
         except Exception as e:
             logging.error("生成i64文件出错->" + e)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("参数1： 二进制程序路径 参数2：i64 文件保存路径")
+        print("参数1： 二进制程序路径 参数2：i64 文件保存路径 可选参数3: ida启动shell脚本 可选参数4: ida安装路径")
         exit(1)
-    main(sys.argv[1], sys.argv[2])
-    print("done.")
-    exit(0)
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
