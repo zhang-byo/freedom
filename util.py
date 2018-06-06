3235b1f52f2f3922054497d75cf5bcee4cff654f
mayuan <mayuanucas@qq.com>
2018-06-06 09:34:54 +0800

    生成i64文件

--- util.py
+++ util.py
@@ -4,6 +4,9 @@ import os
 import json
 import logging, logging.handlers
 
+IDA_ENGINE_PATH = "/usr/local/ida/idal64"
+IDA_START = "./shell/ida_start.sh"
+
 # 程序运行日志文件
 log_dir = "./"
 logfile = log_dir + 'log_util.log'
@@ -69,3 +72,25 @@ def write_json(content, filepath):
         json.dump(content, f)
         return filepath, True
     return None, False
+
+
+def generate_i64(binary_path, idb_store_path, ida_start=IDA_START, ida_engine_path=IDA_ENGINE_PATH):
+    """
+    生成二进制程序的 i64 文件并保存到指定目录(i64文件名可同时指定, 未指定则与二进制程序同名)
+    :param binary_path: 二进制程序路径
+    :param idb_store_path: i64 文件保存路径
+    :param ida_start: 启动 ida 的脚本
+    :param ida_engine_path: idal64 的路径
+    :return: 生成的 i64 文件的路径
+    """
+    if os.path.exists(ida_engine_path):
+        # ida 加载二进制文件并将 ida 的i64文件保存到指定位置
+        os.system('/bin/bash %s %s %s %s' % (ida_start, ida_engine_path, binary_path, idb_store_path))
+        # i64 文件保存在当前目录下时，其默认名字与二进制程序名相同
+        if idb_store_path == "." or idb_store_path == "./":
+            return os.path.basename(binary_path) + ".i64"
+        elif idb_store_path[-4:] == ".i64":
+            # 指定 i64 文件保存路径时, 输入带有文件名后缀 .i64
+            return idb_store_path
+        else:
+            return idb_store_path + ".i64"
